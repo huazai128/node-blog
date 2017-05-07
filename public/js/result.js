5e4e5cd7a1d78d229c81cf6145cd91ae1c3d1b45
huazai128 <941154174@qq.com>
2017-05-07 14:29:52 +0800

    update

--- public/js/result.js
+++ public/js/result.js
@@ -1,7 +1,17 @@
-require("../css/result_tag.css");
+require('../css/result_tag.css');
 let $ = require("jquery");
-
-
+let imgAjax = require('./common/imgAjax');
 $(() => {
-    
+	let flag = {
+			value: true
+		},
+		page = 1,
+		q = window.location.href.split('?')[1].split('=')[1];
+	$(window).on('scroll', () => {
+		if ($('.blog_result_content figure:last').get(0).getBoundingClientRect().top <= $(window).innerHeight() && flag.value) {
+			flag.value = false;
+			imgAjax.getImgMore(flag, page, '/api/showmore/results?q=' + q + '&p=', '.blog_result_content .row');
+			page++;
+		}
+	})
 })
